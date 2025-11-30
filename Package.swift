bdfdba7779a3efc7ed6da4fd9e5134a07f81b954
Grady Zhuo <gradyzhuo@gmail.com>
2025-12-01 01:15:13 +0800

    [UPDATE] add AccessTokenClientMiddleware

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird", from: "2.0.0"),
         .package(url: "https://github.com/beatt83/jose-swift.git", from: "6.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -26,7 +27,8 @@ let package = Package(
             name: "Middleware",
             dependencies: [
                 .product(name: "Hummingbird", package: "hummingbird"),
-                .product(name: "jose-swift", package: "jose-swift")
+                .product(name: "jose-swift", package: "jose-swift"),
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
             ]
         ),
         .testTarget(
---
+++ Sources/Middleware/AccessTokenClient.swift
@@ -0,0 +1,24 @@
+//
+//  AccessTokenClient.swift
+//  Middleware
+//
+//  Created by Grady Zhuo on 2025/12/1.
+//
+
+import Foundation
+import OpenAPIRuntime
+import Hummingbird
+
+public struct AccessTokenMiddleware: ClientMiddleware {
+    
+    public func intercept(_ request: HTTPRequest, body: HTTPBody?, baseURL: URL, operationID: String, next: (HTTPRequest, HTTPBody?, URL) async throws -> (HTTPResponse, HTTPBody?)) async throws -> (HTTPResponse, HTTPBody?) {
+        
+        guard let authorizationHeader = request.headerFields.first(where: { $0.name == .authorization }) else {
+            return try await next(request, body, baseURL)
+        }
+        
+        var newRequest = request
+        newRequest.headerFields.append(authorizationHeader)
+        return try await next(newRequest, body, baseURL)
+    }
+}
